6a496be4664659c6eff2b623aa75155298544a6f
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-14 19:21:55 +0100

    Adding interfaces

---
+++ src/interface/pets.ts
@@ -0,0 +1,12 @@
+export type Pets = {
+    id: number;
+    name: string;
+    species: string;
+    isCute: boolean;
+};
+
+export type ProtoPets = {
+    name: string;
+    species: string;
+    isCute: boolean;
+};
