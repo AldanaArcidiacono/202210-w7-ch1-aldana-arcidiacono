0e2d2976791c2d8e85714c6d7e433d829d45ba54
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-15 20:46:10 +0100

    🏷️ Add type Pets

--- src/interface/pets.ts
+++ src/interface/pets.ts
@@ -1,8 +1,12 @@
-export type Pets = {
+export type Pet = {
     id: number;
     name: string;
     species: string;
     isCute: boolean;
+};
+
+export type Pets = {
+    pets: Pet[];
 };
 
 export type ProtoPets = {
