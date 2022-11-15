420caab0ab18900e70e778f9779aca38a9b015b0
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-11-15 20:46:45 +0100

    ⚡️ Improve router from class

--- src/router/pet.ts
+++ src/router/pet.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { PetsController } from '../controllers/pets.js';
+import { PetFileData } from '../data/pets.file.data.js';
 
 export const petRouter = Router();
 
-const controller = new PetsController();
+const controller = new PetsController(new PetFileData());
 
-petRouter.get('/', controller.getAll);
-petRouter.get('/:id', controller.get);
-petRouter.post('/', controller.post);
-petRouter.patch('/:id', controller.patch);
-petRouter.delete('/:id', controller.delete);
+petRouter.get('/', controller.getAll.bind(controller));
+petRouter.get('/:id', controller.get.bind(controller));
+petRouter.post('/', controller.post.bind(controller));
+petRouter.patch('/:id', controller.patch.bind(controller));
+petRouter.delete('/:id', controller.delete.bind(controller));
